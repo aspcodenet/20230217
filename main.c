0a96855db23e3d65ce806e510b33541776950e07
Stefan <stefan.holmberg@systementor.se>
2023-02-17 12:05:28 +0100

    aaa

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 int plussaTalMindre100(int tal1, int tal2){
     // result += tal1;
@@ -30,8 +31,103 @@ char *hittaLangstaOrdet(char *str1, char *str2, char *str3){
     return str3;
 }
 
+float calculateTaxesOnSalary(float salary){
+    if(salary >= 30000) return salary * 0.33;
+    if(salary < 15000) return salary * 0.12;
+    return salary * 0.28;
+}
+
+bool isVowel(char ch){
+    ch = tolower(ch);
+    if( ch == 'a'  || 
+        ch == 'o' || 
+        ch == 'u' ||
+        ch == 'å' ||
+        ch == 'e' ||
+        ch == 'i' ||
+        ch == 'y' ||
+        ch == 'ä' ||
+        ch == 'ö' ) 
+            return true;
+    return false;
+}
+
+
+int countVowels(char *s){
+    int counter = 0;
+    for(int i=0; i < strlen(s);i++){
+        char ch = s[i];
+        if(isVowel(ch))
+            counter++;
+    }
+    return counter;
+}
+
+
+
+
+int countCharacters(char *s){
+    int counter = 0;
+    for(int i=0; i < strlen(s);i++){
+        char ch = s[i];
+        if(isalnum(ch))
+            counter++;
+    }
+    return counter;
+
+}
+
+bool isConsonant(char ch){
+    return isalpha(ch) && !isVowel(ch);
+}
+
+// input this is fun
+// i non
+void translate(char *input,char *output){
+    //strcpy(output, "");
+    output[0] = 0;
+
+    for(int i = 0; i < strlen(input);i++){
+        char ch = input[i];
+        if(isConsonant(ch)){ 
+            int lastIndex = strlen(output);
+            output[lastIndex] = ch;
+            output[lastIndex+1] = 'o';
+            output[lastIndex+2] = ch;
+            output[lastIndex+3] = 0;
+        } else{
+            int lastIndex = strlen(output);
+            output[lastIndex] = ch;
+            output[lastIndex+1] = 0;
+        }
+    }
+}
+
 
 void main(){
+    char texten[100] = "This is fun";
+    char outtexten[300] = "";
+    translate(texten,outtexten);
+    printf("%s\n",outtexten);
+    //Vokaler
+    char ch = 'A';
+    bool b = isVowel(ch);
+    printf("%c\n", ch); //'a'
+    char txt[100];
+    scanf(" %s", txt);
+    int vowelCount = countVowels(txt);
+    printf("Your text is %d characters and contains %d vowels", 
+        countCharacters(txt), 
+        vowelCount );
+
+
+    //Salary
+    float salary;
+    printf("Monthly salary:");
+    scanf(" %f", &salary);
+    printf("Taxes are: %.2f\n", calculateTaxesOnSalary(salary));
+
+
     //Hitta längsta ordet
     char s1[100];
     char s2[100];
